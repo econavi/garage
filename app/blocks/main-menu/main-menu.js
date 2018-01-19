4741bbc36601e5cbec2f362c90b6f38c20b692e2
Vladimir Kochenov <kochenov.vl@gmail.com>
2018-01-19 23:43:52 +0300

    добавил переменные для - цвета, отступы, размер шрифта

--- app/blocks/main-menu/main-menu.js
+++ app/blocks/main-menu/main-menu.js
@@ -1,10 +1,14 @@
-// получаем ссылку с href из адресной строки
+// получаем ссылки из главного меню
 var elems = document.querySelectorAll('.main-menu__link');
 
 for(var i = 0; i < elems.length; i++) {
-	if(elems[i].getAttribute('href') == window.location.pathname) {
+	// если значение из адресной строки браузера совпадает со значением атрибута href у ссылки, то для ссылки:
+	if(window.location.pathname == elems[i].getAttribute('href')) {
+		// добавляем класс
 		elems[i].classList.add('main-menu__link_active');
+		// удаляем атрибут href
 		elems[i].removeAttribute('href');
+		// устанавливаем стили для курсора мыши по дефолту
 		elems[i].style.cursor = 'default';
 	}
 }
