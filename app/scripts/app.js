15d4bcba4aff19374ff727fa88973bf21da5dcac
Vladimir Kochenov <kochenov.vl@gmail.com>
2018-03-14 21:58:25 +0300

    избавился от jQuery

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,13 @@
 // import svg4everybody from 'svg4everybody';
-import $ from 'jquery';
-// import galleryLightboxHelper from './../blocks/gallery-lightbox/blueimp-helper.js';
-import galleryLightbox from './../blocks/gallery-lightbox/gallery-lightbox';
+// import $ from 'jquery';
+import galleryLightbox from './../blocks/gallery-lightbox/blueimp-gallery';
 import modal from './../blocks/modal/modal';
 import body from './../blocks/body/body';
 import mainNav from './../blocks/main-nav/main-nav';
 import mainMenu from './../blocks/main-menu/main-menu';
 import accordion from './../blocks/accordion/accordion';
 import product from './../blocks/product/product';
+
 // import textarea from './../blocks/textarea/textarea';
 
 // window.onload = () => {
